3b108af6db9f007907ebb171659f527c2c02bdfa
Benjamin Tutu <tutu.benjamin65@gmail.com>
2026-04-29 23:20:39 +0000

    Initial Commit

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    phone = forms.CharField(max_length=20, required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
